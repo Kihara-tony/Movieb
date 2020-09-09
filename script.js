ad09f01e4b61e3ee5a73ee1e21f9be51b25d16ff
Tonkin <tonykiharatonkin6@gmail.com>
2020-09-09 22:24:53 +0300

    commit all changes.

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+ 
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
